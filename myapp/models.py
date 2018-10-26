b5c6435d80352b95bfd4aed3ff6f6eb0dc4bfb3e
razydroid <razydroid@gmail.com>
2018-10-26 10:09:24 -0500

    Changes to include Flask Login Changes

--- myapp/models.py
+++ myapp/models.py
@@ -1,12 +1,17 @@
 from .settings import app
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug import generate_password_hash, check_password_hash
+from flask_login import LoginManager, UserMixin
 
 # db = SQLAlchemy(app)
 
 db = SQLAlchemy(app)
 
-class User(db.Model):
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+class User(UserMixin, db.Model):
     __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
     firstname = db.Column(db.String(80), nullable=False)
@@ -22,6 +27,10 @@ class User(db.Model):
             'lastname': self.lastname
         }
 
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(int(user_id))
+
     def __init__(self,firstname,lastname, email, password):
 
         self.firstname = firstname.title()
@@ -33,7 +42,14 @@ class User(db.Model):
     def find_all_users(self):
         return [User.json(user) for user in User.query.all()]
 
-    def find_user_by_username(_email):
+    def find_user_by_email(_email):
+        user = User.query.filter_by(email=_email).first()
+        if user is None:
+            return True
+        else:
+            return False
+
+    def get_user_by_email(_email):
         return User.json(User.query.filter_by(email=_email).first())
 
     def register_user(_email,_password):
